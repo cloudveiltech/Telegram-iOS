f1530454751e2a536e5c02577b7b72ae24423aab
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2019-07-19 17:50:29 +0400

    remove submodules folder

--- submodules/TelegramPresentationData/Sources/DefaultPresentationStrings.swift
+++
@@ -1,3 +0,0 @@
-import Foundation
-
-public let defaultPresentationStrings = PresentationStrings(primaryComponent: PresentationStringsComponent(languageCode: "en", localizedName: "English", pluralizationRulesCode: nil, dict: NSDictionary(contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource: "Localizable", ofType: "strings", inDirectory: nil, forLocalization: "en")!)) as! [String : String]), secondaryComponent: nil, groupingSeparator: "")
