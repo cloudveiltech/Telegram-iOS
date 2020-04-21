27119e528c469ba470d3b51d85f605e778d0e5b2
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2020-04-21 17:12:44 +0700

    spm fix

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
          .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.1.0")),
-         .package(url: "https://github.com/patriciy/ObjectMapper.git", .upToNextMajor(from: "3.5.3")),
+         .package(url: "https://github.com/patriciy/ObjectMapper.git", .upToNextMajor(from: "3.5.4")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
