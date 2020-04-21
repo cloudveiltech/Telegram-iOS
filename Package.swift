b14d068c1735ffcd3937e92aeb6c39e4ad350d9f
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2020-04-21 15:47:32 +0700

    package manager support

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CloudVeil-securityManager-ios",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CloudVeil-securityManager-ios",
+            targets: ["SecurityManager"]),
+    ],
+    dependencies: [
+         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.1.0")),
+         .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "3.4")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SecurityManager",
+            dependencies: []),
+    ]
+)
