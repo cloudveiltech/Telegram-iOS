5691b0019bcff3c2181bc772f3f7002a735aa5c3
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2021-11-11 17:37:56 +0200

    fixes

--- Telegram/CloudVeilSecurityManager.framework/Headers/CloudVeilSecurityManager-Swift.h
+++ Telegram/CloudVeilSecurityManager.framework/Headers/CloudVeilSecurityManager-Swift.h
@@ -209,6 +209,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 
+
 SWIFT_CLASS("_TtC24CloudVeilSecurityManager14MainController")
 @interface MainController : NSObject
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
@@ -459,6 +460,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="CloudVeilSecurityManager",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
+
 
 
 SWIFT_CLASS("_TtC24CloudVeilSecurityManager14MainController")
--- submodules/ChatListUI/Sources/Node/ChatListNode.swift
+++ submodules/ChatListUI/Sources/Node/ChatListNode.swift
@@ -16,6 +16,62 @@ import SearchUI
 import ChatListSearchItemHeader
 import CloudVeilSecurityManager
 
+//CloudVeil start
+extension DispatchQueue {
+    /**
+     - parameters:
+        - target: Object used as the sentinel for de-duplication.
+        - delay: The time window for de-duplication to occur
+        - work: The work item to be invoked on the queue.
+     Performs work only once for the given target, given the time window. The last added work closure
+     is the work that will finally execute.
+     Note: This is currently only safe to call from the main thread.
+     Example usage:
+     ```
+     DispatchQueue.main.asyncDeduped(target: self, after: 1.0) { [weak self] in
+         self?.doTheWork()
+     }
+     ```
+     */
+    public func asyncDeduped(target: AnyObject, after delay: TimeInterval, execute work: @escaping @convention(block) () -> Void) {
+        let dedupeIdentifier = DispatchQueue.dedupeIdentifierFor(target)
+        if let existingWorkItem = DispatchQueue.workItems.removeValue(forKey: dedupeIdentifier) {
+            existingWorkItem.cancel()
+            NSLog("Deduped work item: \(dedupeIdentifier)")
+        }
+        let workItem = DispatchWorkItem {
+            DispatchQueue.workItems.removeValue(forKey: dedupeIdentifier)
+
+            for ptr in DispatchQueue.weakTargets.allObjects {
+                if dedupeIdentifier == DispatchQueue.dedupeIdentifierFor(ptr as AnyObject) {
+                    work()
+                    NSLog("Ran work item: \(dedupeIdentifier)")
+                    break
+                }
+            }
+        }
+
+        DispatchQueue.workItems[dedupeIdentifier] = workItem
+        DispatchQueue.weakTargets.addPointer(Unmanaged.passUnretained(target).toOpaque())
+
+        asyncAfter(deadline: .now() + delay, execute: workItem)
+    }
+
+}
+
+// MARK: - Static Properties for De-Duping
+private extension DispatchQueue {
+
+    static var workItems = [AnyHashable : DispatchWorkItem]()
+
+    static var weakTargets = NSPointerArray.weakObjects()
+
+    static func dedupeIdentifierFor(_ object: AnyObject) -> String {
+        return "\(Unmanaged.passUnretained(object).toOpaque())." + String(describing: object)
+    }
+}
+//CloudVeil end
+
 public enum ChatListNodeMode {
     case chatList
     case peers(filter: ChatListNodePeersFilter, isSelecting: Bool, additionalCategories: [ChatListNodeAdditionalCategory], chatListFilters: [ChatListFilter]?)
@@ -849,7 +905,7 @@ public final class ChatListNode: ListView {
             
             let (rawEntries, isLoading) = chatListNodeEntriesForView(update.view, state: state, savedMessagesPeer: savedMessagesPeer, foundPeers: state.foundPeers, hideArchivedFolderByDefault: hideArchivedFolderByDefault, displayArchiveIntro: displayArchiveIntro, mode: mode)
             //CloudVeil start
-            DispatchQueue.main.sync {
+            DispatchQueue.main.asyncDeduped(target: self, after: 0.5) {
                 let appState = UIApplication.shared.applicationState
                 if appState != UIApplication.State.background {
                     self.backgroundQueue.async {
@@ -1467,6 +1523,7 @@ public final class ChatListNode: ListView {
                                   }
                                    if proceededGroupIds.count == groups.count + channels.count {
                                        MainController.shared.getSettings(groups: groups, bots: bots, channels: channels)
+                                       proceededGroupIds = []
                                    }
                                })
                            }
