28a07f99ae0dee941ad4fb7ca21973cde9fa4ba0
duyhungtnn <duyhung71089@gmail.com>
2024-11-29 00:20:40 +0700

    Update RequestUserPhotos.swift

--- submodules/TelegramCore/Sources/TelegramEngine/Peers/RequestUserPhotos.swift
+++ submodules/TelegramCore/Sources/TelegramEngine/Peers/RequestUserPhotos.swift
@@ -25,12 +25,12 @@ public struct TelegramPeerPhoto {
 
 // CloudVeil start
 // TODO: KJ: recheck me
-public func requestPeerPhotos(postbox: Postbox, network: Network, peerId: PeerId) -> Signal<[TelegramPeerPhoto], NoError> {
-    return _internal_requestPeerPhotos(postbox: postbox, network: network, peerId: peerId)
+public func requestPeerPhotos(accountPeerId: PeerId, postbox: Postbox, network: Network, peerId: PeerId) -> Signal<[TelegramPeerPhoto], NoError> {
+    return _internal_requestPeerPhotos(accountPeerId: accountPeerId, postbox: postbox, network: network, peerId: peerId)
 }
 // CloudVeil end
 
-func _internal_requestPeerPhotos(postbox: Postbox, network: Network, peerId: PeerId) -> Signal<[TelegramPeerPhoto], NoError> {
+func _internal_requestPeerPhotos(accountPeerId: PeerId, postbox: Postbox, network: Network, peerId: PeerId) -> Signal<[TelegramPeerPhoto], NoError> {
     return postbox.transaction{ transaction -> Peer? in
         return transaction.getPeer(peerId)
     }
