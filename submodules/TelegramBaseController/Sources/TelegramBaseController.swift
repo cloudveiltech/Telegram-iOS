6ad3f1aa7d847af579be8f4f60a94faedbfc5c09
duyhungtnn <duyhung71089@gmail.com>
2024-12-26 11:46:36 +0700

    fix: correct channel id

--- submodules/TelegramBaseController/Sources/TelegramBaseController.swift
+++ submodules/TelegramBaseController/Sources/TelegramBaseController.swift
@@ -1193,8 +1193,7 @@ open class TelegramBaseController: ViewController, KeyShortcutResponder {
     }
     
     private static func openUnblockRequest(peerView: Peer, context: AccountContext, controller: ViewController, presentationData: PresentationData) {
-        let conversationId = peerView.id.id._internalGetInt64Value()
-        
+        let conversationId = -NSInteger(peerView.id.id._internalGetInt64Value())
         let userId = TGUserController.userID
         let url = "https://messenger.cloudveil.org/unblock/\(userId)/\(conversationId)"
 
