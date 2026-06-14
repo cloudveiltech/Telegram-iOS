0d9642a728f73fa4f434ce048b53c336dea50b21
duyhungtnn <duyhung71089@gmail.com>
2026-06-14 16:14:49 +0700

    chore: update bazel version

--- build-system/NewMake/main.go
+++ build-system/NewMake/main.go
@@ -143,7 +143,7 @@ func usage(w io.Writer) {
 	tw.Flush()
 }
 
-const BAZEL_VERSION_ENV = "USE_BAZEL_VERSION=7.3.1"
+const BAZEL_VERSION_ENV = "USE_BAZEL_VERSION=8.4.2"
 
 // runCmd runs an arbitrary command, passing through its stdout and stderr to ours
 func runCmd(cmd string, args ...string) error {
